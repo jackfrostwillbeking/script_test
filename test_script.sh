be4113c061a1852824cd5d3c3812b83176e1ce21
kawasakiya <31604636+jackfrostwillbeking@users.noreply.github.com>
2018-03-09 19:17:51 +0900

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -6,12 +6,7 @@ REFRESH_TOKEN="1/j06fxt0YkYtrQEBUvKd_ILHsX_l2T7LN84FRG1uuZoeUQTEz3fEbIOXusFQoJRJ
 CLIENT_ID="761866041142-ss6v9qaiq0oel3esqm81enbf0k24dvh1.apps.googleusercontent.com"
 CLIENT_SECRET="H4bQe1RaElK6WXUfirfHSdEF"
 META_DATA="{\"title\": \"$FILE\",\"mimeType\": \"text/plain\",\"description\": \"Uploaded From Curl\"}"
-GET_TOKEN_RESULT=$(curl -X POST https://accounts.google.com/o/oauth2/token \
--d "code=4/AABAfsXzHlu1ZdihEuniS4JkHqebX5OE-rvdTJbF2nMCPtQY0ybBO8k" \
--d "client_id=761866041142-ss6v9qaiq0oel3esqm81enbf0k24dvh1.apps.googleusercontent.com" \
--d "client_secret=H4bQe1RaElK6WXUfirfHSdEF" \
--d "redirect_uri=urn:ietf:wg:oauth:2.0:oob" \
--d "grant_type=authorization_code")
+
 function upload_file() {
     curl -X POST https://www.googleapis.com/upload/drive/v2/files?uploadType=multipart \
     -H "Authorization: Bearer $ACCESS_TOKEN" \
